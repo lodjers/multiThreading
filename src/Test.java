096ce84fc96e14130b8e916e6954b88318e72064
lodjers <officialkovtun@gmail.com>
2025-04-14 12:23:33 +0300

    Реализовали остановку потока (thread was interrupted)

--- src/Test.java
+++ src/Test.java
@@ -9,110 +9,27 @@ import java.util.concurrent.locks.ReentrantLock;
 
 public class Test {
     public static void main(String[] args) throws InterruptedException {
-
-        Runner runner = new Runner();
-        Thread thread1 = new Thread(new Runnable() {
+        Thread thread = new Thread(new Runnable() {
             @Override
             public void run() {
-                runner.firstThread();
+                Random random = new Random();
+                for (int i = 0; i < 1_000_000_000; i++) {
+                    if(Thread.currentThread().isInterrupted()) {
+                        System.out.println("Thread was interrupted");
+                        break;
+                    }
+                    Math.sin(random.nextDouble());
+                }
             }
         });
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                runner.secondThread();
-            }
-        });
+        System.out.println("Starting thread");
 
-        thread1.start();
-        thread2.start();
+        thread.start();
 
-        thread1.join();
-        thread2.join();
+        Thread.sleep(1000);
+        thread.interrupt();
 
-        runner.finished();
-    }
-
-}
-class Runner {
-    private Account account1 = new Account();
-    private Account account2 = new Account();
-    private Lock lock1 = new ReentrantLock();
-    private Lock lock2 = new ReentrantLock();
-
-    private void takeLocks(Lock lock1, Lock lock2) {
-        boolean firstLockTaken = false;
-        boolean secondLockTaken = false;
-        while (true) {
-            try {
-                firstLockTaken = lock1.tryLock();
-                secondLockTaken = lock2.tryLock();
-            } finally {
-                if (firstLockTaken && secondLockTaken) {
-                    return;
-                }
-                if (firstLockTaken) {
-                    lock1.unlock();
-                }
-                if (secondLockTaken) {
-                    lock2.unlock();
-                }
-            }
-            try {
-                Thread.sleep(1);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-    public void firstThread() {
-        Random random = new Random();
-        for(int i = 0; i < 10000; i++) {
-            takeLocks(lock1, lock2);
-            try {
-                Account.transfer(account1, account2, random.nextInt(100));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
-        }
-    }
-    public void secondThread() {
-        Random random = new Random();
-        for(int i = 0; i < 10000; i++) {
-            takeLocks(lock2, lock1);
-            try {
-                Account.transfer(account2, account1, random.nextInt(100));
-            } finally {
-                lock1.unlock();
-                lock2.unlock();
-            }
-        }
-    }
-    public void finished() {
-        System.out.println(account1.getBalance());
-        System.out.println(account2.getBalance());
-        System.out.println("Total balance " + (account1.getBalance() + account2.getBalance()));
-    }
-}
-
-class Account {
-    private int balance = 10000;
-
-    public void deposit(int amount) {
-        balance += amount;
-    }
-
-    public void withdraw(int amount) {
-        balance -= amount;
-    }
-
-    public int getBalance() {
-        return balance;
-    }
-
-    public static void transfer(Account acc1, Account acc2, int amount) {
-        acc1.withdraw(amount);
-        acc2.deposit(amount);
+        thread.join();
+        System.out.println("Thread has finished");
     }
 }
